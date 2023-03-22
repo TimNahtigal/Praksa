9876064b2b890e2b9872ac1a531322e12c253272
NahtigalTim <47721662+TimNahtigal@users.noreply.github.com>
2023-03-22 19:47:54 +0100

    Add files via upload

---
+++ PraksaSQL.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE `praksa` ;
+
+USE `praksa`;
+
+CREATE TABLE `uservisits` (
+  `idUserVisits` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NULL DEFAULT NULL,
+  `ip` varchar(20) DEFAULT NULL,
+  `url` varchar(100) DEFAULT NULL,
+  `domain` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idUserVisits`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
