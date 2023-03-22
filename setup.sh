20f3f6dccd59c52fbedeb4811445fb9364a6fef5
NahtigalTim <47721662+TimNahtigal@users.noreply.github.com>
2023-03-22 19:48:33 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -27,18 +27,7 @@ mysql -e "DROP DATABASE test"
 # Make our changes take effect
 mysql -e "FLUSH PRIVILEGES"
 
-mysql -e "CREATE DATABASE `praksa`;"
-
-mysql -e "CREATE TABLE praksa.`uservisits` (
-  `idUserVisits` int(11) NOT NULL AUTO_INCREMENT,
-  `timestamp` timestamp NULL DEFAULT NULL,
-  `ip` varchar(20) DEFAULT NULL,
-  `url` varchar(100) DEFAULT NULL,
-  `domain` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idUserVisits`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;"
-
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -proot -e "source PraksaSQL.sql"
 
 
 cat >> /etc/systemd/system/PraksaStreznik.service << EOF
